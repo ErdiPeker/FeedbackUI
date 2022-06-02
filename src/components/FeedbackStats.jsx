5150e7708c81ccc9b6f82a26b0cd6524c1c093c5
Erdi Peker <erdipeker@gmail.com>
2022-06-02 10:54:08 +0300

    initial

---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
+
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext);
+
+  let average = feedback.reduce((acc, cur) => {
+    return acc + cur.rating / feedback.length;
+  }, 0);
+
+  average = average.toFixed(1).replace(/[.,]0$/, "");
+  return (
+    <div className="feedback-stats">
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {average} </h4>
+    </div>
+  );
+}
+export default FeedbackStats;
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: "1",
+    rating: 10,
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio inventore asperiores ipsam laudantium eveniet, fuga hic. Nihil, sunt. Magnam praesentium exercitationem optio consectetur, mollitia voluptas aut ab dolorem expedita delectus.",
+  },
+  {
+    id: "2",
+    rating: 9,
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio inventore asperiores ipsam laudantium eveniet, fuga hic. Nihil, sunt. Magnam praesentium exercitationem optio consectetur, mollitia voluptas aut ab dolorem expedita delectus.",
+  },
+  {
+    id: "3",
+    rating: 8,
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio inventore asperiores ipsam laudantium eveniet, fuga hic. Nihil, sunt. Magnam praesentium exercitationem optio consectetur, mollitia voluptas aut ab dolorem expedita delectus.",
+  },
+];
+
+export default FeedbackData;
